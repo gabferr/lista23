01b6fdb2f9935b17278935c5a3a2d3abb7869435
gabferr <gabrielferrandin@hotmail.com>
2023-02-11 09:22:07 -0300

    primeiro commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO livro (titulo, paginas, autor) VALUES ('O senhor dos anéis', 1000, 'Tolkien');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('Spring Boot', 328, 'Mark Heckler');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('O Principe', 1000, 'Maquiavel');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('Boot Spring', 520, 'Eu mesmo');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('Hacker sec', 502, 'Alguem por ai');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('Em testes', 328, 'Tolkiem');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('Pequeno Principe', 965, 'Todos nós');
+INSERT INTO livro (titulo, paginas, autor) VALUES ('Em testes', 236, 'Zé ninguem 2');
